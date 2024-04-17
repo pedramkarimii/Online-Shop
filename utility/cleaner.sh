771b8ef12a68ecd2f3744c8eb155459e2ad76593
pedramkarimii <pedram.9060@gmail.com>
2024-04-17 23:37:08 +0330

    :sparkles: add cleaner db, create  makemigrations, migrate and createsuperuser

---
+++ utility/cleaner.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+rm -rf db.sqlite3
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+
+python manage.py check
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser --phone_number 09128355747 --username pedramkarimi --email pedram.9060@gmail.com
+
