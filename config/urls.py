dad945f6c780393d859beaadf775fb2aebf40515
pedramkarimii <pedram.9060@gmail.com>
2024-04-18 00:04:17 +0330

    :sparkles: Customize Django admin interface titles and Check if the application is in debug mode Serve static files at STATIC_URL and MEDIA_URL.

--- config/urls.py
+++ config/urls.py
@@ -1,20 +1,27 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include
-from django.urls import path
+from django.urls import path, include
+
+"""Define URL patterns for the entire application."""
 urlpatterns = [
     path("", include("apps.core.urls")),
     path("", include("apps.account.urls")),
-    path("", include("apps.customer.urls")),
     path("", include("apps.order.urls")),
     path("", include("apps.product.urls")),
 ]
 
+"""Check if the application is in debug mode."""
 if settings.DEBUG:
     from django.conf.urls.static import static
 
+    """Add URL pattern for accessing the Django admin site."""
     urlpatterns += (
-        [path("admin/", admin.site.urls)]
-        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+            [path("admin/", admin.site.urls)]
+            + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+            + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     )
+
+    """Customize Django admin interface titles."""
+    admin.site.site_header = 'Online Shop'
+    admin.site.site_title = 'Online Shop Administration'
+    admin.site.index_title = 'Welcome To Online Shop Administration'
