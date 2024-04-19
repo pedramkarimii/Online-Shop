9f73928e01e01ec1f485e661ddc27f94b646cfab
pedramkarimii <pedram.9060@gmail.com>
2024-04-19 23:33:29 +0330

    :ambulance: change api and template: urls_api and urls_template

--- apps/core/urls/__init__.py
+++ apps/core/urls/__init__.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("api/", include("apps.account.urls.api")),
-    path("template/", include("apps.account.urls.template")),
+    path("api/", include("apps.core.urls.urls_api")),
+    path("template/", include("apps.core.urls.urls_template")),
 ]
