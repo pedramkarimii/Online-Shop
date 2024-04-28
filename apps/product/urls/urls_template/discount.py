c91fa94a904ce0be5a1c625876b17f0a0110aa40
pedramkarimii <pedram.9060@gmail.com>
2024-04-29 02:34:14 +0330

    :sparkles: add URL patterns for discount create, update, and delete views

--- apps/product/urls/urls_template/discount.py
+++ apps/product/urls/urls_template/discount.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from apps.product.views import views_template
+from apps.product.views.views_template import views_discount_create
 
 urlpatterns = [
-    path('discount-create/', views_template.DiscountCreateView.as_view(), name='discount_create'),
-    # path('discount-detail/<int:pk>/', views_template.DiscountDetailView.as_view(), name='discount_detail'),
-    path('discount-update/<int:pk>/', views_template.DiscountUpdateView.as_view(), name='discount_update'),
-    path('discount-delete/<int:pk>/', views_template.DiscountDeleteView.as_view(), name='discount_delete'),
+    path('discount-create/', views_discount_create.DiscountCreateView.as_view(), name='discount_create'),
+    # path('discount-detail/<int:pk>/', views_product.DiscountDetailView.as_view(), name='discount_detail'),
+    path('discount-update/<int:pk>/', views_discount_create.DiscountUpdateView.as_view(), name='discount_update'),
+    path('discount-delete/<int:pk>/', views_discount_create.DiscountDeleteView.as_view(), name='discount_delete'),
 ]
