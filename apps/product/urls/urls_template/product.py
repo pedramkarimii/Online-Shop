309da40c33465937b3a7fc03aa25dc382d113478
pedramkarimii <pedram.9060@gmail.com>
2024-04-29 02:42:06 +0330

    :sparkles: add URL patterns for product create, detail, update, and delete views

--- apps/product/urls/urls_template/product.py
+++ apps/product/urls/urls_template/product.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from apps.product.views import views_template
+from apps.product.views.views_template import views_product
 
 urlpatterns = [
 
-    path('product-create/', views_template.ProductCreateView.as_view(), name='product_create'),
-    # path('product-detail/<int:pk>/', views_template.ProductDetailView.as_view(), name='product_detail'),
-    path('product-update/<int:pk>/', views_template.ProductUpdateView.as_view(), name='product_update'),
-    path('product-delete/<int:pk>/', views_template.ProductDeleteView.as_view(), name='product_delete'),
+    path('product-create/', views_product.ProductCreateView.as_view(), name='product_create'),
+    path('product-detail/<int:pk>/', views_product.ProductDetailView.as_view(), name='product_detail'),
+    path('product-update/<int:pk>/', views_product.ProductUpdateView.as_view(), name='product_update'),
+    path('product-delete/<int:pk>/', views_product.ProductDeleteView.as_view(), name='product_delete'),
 ]
