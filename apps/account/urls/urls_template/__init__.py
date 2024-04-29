7d78c4f3f9fa0786a877197f2206e63a0a96c845
pedramkarimii <pedram.9060@gmail.com>
2024-04-30 03:17:30 +0330

    :fire: Refactor URL patterns for account app templates

---
+++ apps/account/urls/urls_template/__init__.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("", include("apps.account.urls.urls_template.address")),
+    path("", include("apps.account.urls.urls_template.auth")),
+    path("", include("apps.account.urls.urls_template.user")),
+    path("", include("apps.account.urls.urls_template.discount_cod")),
+    path("", include("apps.account.urls.urls_template.profile")),
+    path("", include("apps.account.urls.urls_template.resat_password")),
+]
