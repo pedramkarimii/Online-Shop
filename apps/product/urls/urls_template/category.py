954d424f0efb0b22d41f06bca774f994cc1794ea
pedramkarimii <pedram.9060@gmail.com>
2024-04-30 03:26:23 +0330

    :sparkles: add URL detail

--- apps/product/urls/urls_template/category.py
+++ apps/product/urls/urls_template/category.py
@@ -3,7 +3,7 @@ from apps.product.views.views_template import views_category
 
 urlpatterns = [
     path('category-create/', views_category.CategoryCreateView.as_view(), name='category_create'),
-    # path('category-detail/<int:pk>/', views_category.CategoryDetailView.as_view(), name='category_detail'),
+    path('category-detail/<int:pk>/', views_category.CategoryDetailView.as_view(), name='category_detail'),
     path('category-update/<int:pk>/', views_category.CategoryUpdateView.as_view(), name='category_update'),
     path('category-delete/<int:pk>/', views_category.CategoryDeleteView.as_view(), name='category_delete'),
 ]
