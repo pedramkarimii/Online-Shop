2f2040743fe6f21ed6e569a98351919b7cacdcf1
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 21:41:35 +0330

    :sparkles: add role

--- apps/account/urls/urls_template/__init__.py
+++ apps/account/urls/urls_template/__init__.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("apps.account.urls.urls_template.address")),
+    path("", include("apps.account.urls.urls_template.role")),
     path("", include("apps.account.urls.urls_template.auth")),
     path("", include("apps.account.urls.urls_template.user")),
     path("", include("apps.account.urls.urls_template.discount_cod")),
