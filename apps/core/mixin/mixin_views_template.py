5ee213580aa7ed306b0aa9120ac3bc897da70fea
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 23:34:18 +0330

    :sparkles:  Added SellerPermissionRequiredMixinView

--- apps/core/mixin/mixin_views_template.py
+++ apps/core/mixin/mixin_views_template.py
@@ -1,7 +1,10 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import View
+
+from apps.product.models import Brand
 
 
 class HttpsOptionNotLogoutMixin(View):
@@ -84,3 +87,35 @@ class HttpsOptionLoginMixin(View):
         """
         super().http_method_not_allowed(request, *args, **kwargs)
         return render(request, self.template_http_method_not_allowed)
+
+
+class SellerPermissionRequiredMixinView(PermissionRequiredMixin, HttpsOptionNotLogoutMixin):
+    http_method_names = ['get', 'post']
+    permission_required = (
+        'Seller',
+    )
+
+    def dispatch(self, request, *args, **kwargs):
+        self.brand_instance = get_object_or_404(Brand, pk=kwargs['pk'])  # noqa
+        if not self.has_permission():
+            messages.error(request, 'You do not have permission.', extra_tags='error')
+            return redirect(self.next_page_home)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_permission_required(self):
+        """
+        Returns the list of permissions required to access the view.
+        """
+        return self.permission_required
+
+    def has_permission(self):
+        if self.request.user.is_authenticated and (
+                self.request.user.is_superuser
+                or self.request.user.is_staff
+                or self.request.user.is_active
+                or self.request.user.groups.filter(name='Supervisor').exists()
+                or self.request.user.groups.filter(name='Seller').exists()
+                and self.brand_instance.user == self.request.user):
+            return True
+        else:
+            return False
