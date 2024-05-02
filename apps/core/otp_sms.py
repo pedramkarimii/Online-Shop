d8f714e6b0f85096208b1114b1b8eab6ea6e60e5
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 22:43:12 +0330

    :sparkles: Updated Comment code: class OtpCode

--- apps/core/otp_sms.py
+++ apps/core/otp_sms.py
@@ -1,13 +1,13 @@
 import redis
 from kavenegar import *
 import random
-import time
-import aioredis
+# import time
+# import aioredis
 
 
 class CodeGenerator:
     def __init__(self):
-        self.redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+        self.redis_client = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
 
     def generate_and_store_code(self, phone_number):
         """
@@ -36,35 +36,35 @@ class CodeGenerator:
         """
         return self.redis_client.get(phone_number)
 
-
-class OtpCode:
-    def __init__(self, redis_uri='redis://localhost'):
-        self.redis_uri = redis_uri
-
-    async def generate_code(self, phone_number):
-        async with aioredis.from_url(self.redis_uri, encoding='utf-8') as redis_client:
-            request_key = f'request_count:{phone_number}'
-            last_request_key = f'last_request:{phone_number}'
-
-            request_count = await redis_client.get(request_key)
-            if request_count is not None and int(request_count) >= 3:
-                last_request_time = await redis_client.get(last_request_key)
-                if last_request_time is not None and time.time() - float(last_request_time) < 600:
-                    raise ValueError("Exceeded maximum request limit. Please try again later.")
-
-            otp = ''.join(random.choices("0123456789", k=6))
-
-            await redis_client.setex(f'otp_code:{phone_number}', 120, otp)
-
-            await redis_client.incr(request_key)
-            await redis_client.expire(request_key, 600)
-            await redis_client.set(last_request_key, time.time(), expire=600)
-
-            return otp
-
-    async def get_code(self, phone_number):
-        async with aioredis.from_url(self.redis_uri, encoding='utf-8') as redis_client:
-            return await redis_client.get(f'otp_code:{phone_number}')
+#
+# class OtpCode:
+#     def __init__(self, redis_uri='redis://localhost'):
+#         self.redis_uri = redis_uri
+#
+#     async def generate_code(self, phone_number):
+#         async with aioredis.from_url(self.redis_uri, encoding='utf-8') as redis_client:
+#             request_key = f'request_count:{phone_number}'
+#             last_request_key = f'last_request:{phone_number}'
+#
+#             request_count = await redis_client.get(request_key)
+#             if request_count is not None and int(request_count) >= 3:
+#                 last_request_time = await redis_client.get(last_request_key)
+#                 if last_request_time is not None and time.time() - float(last_request_time) < 600:
+#                     raise ValueError("Exceeded maximum request limit. Please try again later.")
+#
+#             otp = ''.join(random.choices("0123456789", k=6))
+#
+#             await redis_client.setex(f'otp_code:{phone_number}', 120, otp)
+#
+#             await redis_client.incr(request_key)
+#             await redis_client.expire(request_key, 600)
+#             await redis_client.set(last_request_key, time.time(), expire=600)
+#
+#             return otp
+#
+#     async def get_code(self, phone_number):
+#         async with aioredis.from_url(self.redis_uri, encoding='utf-8') as redis_client:
+#             return await redis_client.get(f'otp_code:{phone_number}')
 
 
 def send_otp_code(phone_number, code):
