3a11033d8f139733e2d364b749b1ad8dc0dd75f1
pedramkarimii <pedram.9060@gmail.com>
2024-05-03 00:10:39 +0330

    :bug:  Change add PK in URL user update

--- apps/account/urls/urls_template/user.py
+++ apps/account/urls/urls_template/user.py
@@ -2,7 +2,7 @@ from django.urls import path
 from apps.account.views.views_template import views_user
 
 urlpatterns = [
-    path('user-update/', views_user.UserUpdateView.as_view(), name='user_update'),
+    path('user-update/<int:pk>/', views_user.UserUpdateView.as_view(), name='user_update'),
     path("user-change-password/", views_user.ChangePasswordView.as_view(), name="user_change_password"),
     path('user/delete/<int:pk>/', views_user.UserDeleteView.as_view(), name='user_delete'),
 ]
