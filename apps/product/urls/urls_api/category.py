dbccbe637518c3855a3a34f54331f454dae7d3ef
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 23:12:15 +0330

    :sparkles:  Add simple url api

---
+++ apps/product/urls/urls_api/category.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from apps.product.views import views_template
+
+urlpatterns = [
+    # path('category-create/', views_template.CategoryCreateView.as_view(), name='category_create'),
+    # # path('category-detail/<int:pk>/', views_template.CategoryDetailView.as_view(), name='category_detail'),
+    # path('category-update/<int:pk>/', views_template.CategoryUpdateView.as_view(), name='category_update'),
+    # path('category-delete/<int:pk>/', views_template.CategoryDeleteView.as_view(), name='category_delete'),
+]
