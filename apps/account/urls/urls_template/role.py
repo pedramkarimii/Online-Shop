559a2dea2b2c1199ef4449249bf4cf0a3376a76e
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 22:49:54 +0330

    :sparkles:  Add URL patterns for role

---
+++ apps/account/urls/urls_template/role.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from apps.account.views.views_template import views_role
+
+urlpatterns = [
+    path('role-create/', views_role.RoleCreateView.as_view(), name='role_create'),
+    # path('admin-role-list/', views_role.AdminRoleDetailView.as_view(),
+    #      name='admin_role_list'),
+    # path('role-detail/<int:pk>/', views_role.RoleDetailView.as_view(), name='role_detail'),
+    path('role-update/<int:pk>/', views_role.RoleUpdateView.as_view(), name='role_update'),
+    # path('role-delete/<int:pk>/', views_role.RoleDeleteView.as_view(), name='role_delete'),
+]
