8a4b5db6c667295745207ad6c5d2c7fc965782df
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 21:56:14 +0330

    :sparkles: Added url Brand detail show Brand list

--- apps/product/urls/urls_template/brand.py
+++ apps/product/urls/urls_template/brand.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from apps.product.views.views_template import views_brand
+
 urlpatterns = [
     path('brand-create/', views_brand.BrandCreateView.as_view(), name='brand_create'),
-    # path('brand-detail/<int:pk>/', views_product.BrandDetailView.as_view(), name='brand_detail'),
+    path('admin-seller-brand-list/', views_brand.AdminOrSellerBrandListView.as_view(),
+         name='admin_or_seller_brand_list'),
+    path('brand-detail/<int:pk>/', views_brand.BrandDetailView.as_view(), name='brand_detail'),
     path('brand-update/<int:pk>/', views_brand.BrandUpdateView.as_view(), name='brand_update'),
     path('brand-delete/<int:pk>/', views_brand.BrandDeleteView.as_view(), name='brand_delete'),
 ]
