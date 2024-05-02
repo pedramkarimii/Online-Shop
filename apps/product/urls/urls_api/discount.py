b9abeb021b1cbbd9cffc4e49152117f2921c1276
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 23:12:43 +0330

    :sparkles:  Add simple url api

---
+++ apps/product/urls/urls_api/discount.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from apps.product.views import views_template
+
+urlpatterns = [
+    # path('discount-create/', views_template.DiscountCreateView.as_view(), name='discount_create'),
+    # # path('discount-detail/<int:pk>/', views_template.DiscountDetailView.as_view(), name='discount_detail'),
+    # path('discount-update/<int:pk>/', views_template.DiscountUpdateView.as_view(), name='discount_update'),
+    # path('discount-delete/<int:pk>/', views_template.DiscountDeleteView.as_view(), name='discount_delete'),
+]
