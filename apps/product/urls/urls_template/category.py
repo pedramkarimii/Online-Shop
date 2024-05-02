f08db45485f7838f53fcc5ec95d99c442e2d9c2d
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 21:59:25 +0330

    :sparkles: Added Url show category list

--- apps/product/urls/urls_template/category.py
+++ apps/product/urls/urls_template/category.py
@@ -3,6 +3,7 @@ from apps.product.views.views_template import views_category
 
 urlpatterns = [
     path('category-create/', views_category.CategoryCreateView.as_view(), name='category_create'),
+    path('admin-category-list/', views_category.AdminCategoryListView.as_view(), name='admin_category_list'),
     path('category-detail/<int:pk>/', views_category.CategoryDetailView.as_view(), name='category_detail'),
     path('category-update/<int:pk>/', views_category.CategoryUpdateView.as_view(), name='category_update'),
     path('category-delete/<int:pk>/', views_category.CategoryDeleteView.as_view(), name='category_delete'),
