887489ca386b425d7d15214e33e86d2b1ad6d85e
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 21:46:01 +0330

    :sparkles: Added  URL patterns for bucket endpoints: product, category, brand, discount, and warehouse_keeper

---
+++ apps/product/urls/urls_bucket/__init__.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("", include("apps.product.urls.urls_bucket.product")),
+    path("", include("apps.product.urls.urls_bucket.category")),
+    path("", include("apps.product.urls.urls_bucket.brand")),
+    path("", include("apps.product.urls.urls_bucket.discount")),
+    path("", include("apps.product.urls.urls_bucket.warehouse_keeper")),
+]
