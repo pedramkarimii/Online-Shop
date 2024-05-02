dce2f27b1d01c7b5e723baefd2792cdaa2e81fbb
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 22:05:32 +0330

    :sparkles: Added  Url show discount with list

--- apps/product/urls/urls_template/discount.py
+++ apps/product/urls/urls_template/discount.py
@@ -3,7 +3,8 @@ from apps.product.views.views_template import views_discount_create
 
 urlpatterns = [
     path('discount-create/', views_discount_create.DiscountCreateView.as_view(), name='discount_create'),
-    # path('discount-detail/<int:pk>/', views_product.DiscountDetailView.as_view(), name='discount_detail'),
+    path('admin-discount-list/', views_discount_create.AdminDiscountListView.as_view(), name='admin_discount_list'),
+    path('discount-detail/<int:pk>/', views_discount_create.DiscountDetailView.as_view(), name='discount_detail'),
     path('discount-update/<int:pk>/', views_discount_create.DiscountUpdateView.as_view(), name='discount_update'),
     path('discount-delete/<int:pk>/', views_discount_create.DiscountDeleteView.as_view(), name='discount_delete'),
 ]
