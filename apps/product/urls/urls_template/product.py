062d5b4ff442d825499543953a5cad5616ca7bba
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 22:45:00 +0330

    :sparkles: Add URL show product list with permissions admin or seller

--- apps/product/urls/urls_template/product.py
+++ apps/product/urls/urls_template/product.py
@@ -4,6 +4,8 @@ from apps.product.views.views_template import views_product
 urlpatterns = [
 
     path('product-create/', views_product.ProductCreateView.as_view(), name='product_create'),
+    path('admin-seller-product-list/', views_product.AdminOrSellerProductListView.as_view(),
+         name='admin_or_seller_product_list'),
     path('product-detail/<int:pk>/', views_product.ProductDetailView.as_view(), name='product_detail'),
     path('product-update/<int:pk>/', views_product.ProductUpdateView.as_view(), name='product_update'),
     path('product-delete/<int:pk>/', views_product.ProductDeleteView.as_view(), name='product_delete'),
