0b88ce3df31f052259816772de35e7dcbb10a573
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 23:09:22 +0330

    :sparkles: Added URLs for creating, updating, and deleting warehouse keepers

--- apps/product/urls/urls_template/warehouse_keeper.py
+++ apps/product/urls/urls_template/warehouse_keeper.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from apps.product.views import views_template
+from apps.product.views.views_template import views_warehouse_keeper
 
 urlpatterns = [
-    path('warehouse-keeper-create/', views_template.WarehouseKeeperCreateView.as_view(),
+    path('warehouse-keeper-create/', views_warehouse_keeper.WarehouseKeeperCreateView.as_view(),
          name='warehouse_keeper_create'),
-    # path('warehouse-keeper-detail/<int:pk>/', views_template.WarehouseKeeperDetailView.as_view(), name='warehouse_keeper_detail'),
-    path('warehouse-keeper-update/<int:pk>/', views_template.WarehouseKeeperUpdateView.as_view(),
+    path('admin-warehouse-keeper-list/', views_warehouse_keeper.AdminWarehouseKeeperListView.as_view(),
+         name='admin_warehouse_keeper_list'),
+    path('warehouse-keeper-detail/<int:pk>/', views_warehouse_keeper.WarehouseKeeperDetailView.as_view(),
+         name='warehouse_keeper_detail'),
+    path('warehouse-keeper-update/<int:pk>/', views_warehouse_keeper.WarehouseKeeperUpdateView.as_view(),
          name='warehouse_keeper_update'),
-    path('warehouse-keeper-delete/<int:pk>/', views_template.WarehouseKeeperDeleteView.as_view(),
+    path('warehouse-keeper-delete/<int:pk>/', views_warehouse_keeper.WarehouseKeeperDeleteView.as_view(),
          name='warehouse_keeper_delete'),
 ]
