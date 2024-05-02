3c33807df99547abd370217aad3821d7f20d1796
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 22:06:38 +0330

    :sparkles: Add URLs for discount code creation, listing, detail, update, and deletion

---
+++ apps/account/urls/urls_template/discount_cod.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from apps.account.views.views_template import views_discount
+
+urlpatterns = [
+    path('discount-cod-create/', views_discount.DiscountCodCreateView.as_view(), name='discount_cod_create'),
+    path('admin-discount-cod-list/', views_discount.AdminDiscountCodDetailView.as_view(), name='admin_discount_cod_list'),
+    path('discount-cod-detail/<int:pk>/', views_discount.DiscountCodDetailView.as_view(), name='discount_cod_detail'),
+    path('discount-cod-update/<int:pk>/', views_discount.DiscountCodUpdateView.as_view(), name='discount_cod_update'),
+    path('discount-cod-delete/<int:pk>/', views_discount.DiscountCodDeleteView.as_view(), name='discount_cod_delete'),
+]
