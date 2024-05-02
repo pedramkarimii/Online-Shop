b92332c88a76938cc75ceb44632188fb9bab83cb
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 21:48:29 +0330

    :sparkles: Added address detail

--- apps/account/urls/urls_template/address.py
+++ apps/account/urls/urls_template/address.py
@@ -3,7 +3,7 @@ from apps.account.views.views_template import views_address
 
 urlpatterns = [
     path("address-create/", views_address.AddressCreateView.as_view(), name="address_create"),
-    # path("address-detail/<int:pk>/", views_template.AddressDetailView.as_view(), name="address_detail"),
+    path("address-detail/<int:pk>/", views_address.AddressDetailView.as_view(), name="address_detail"),
     path("address-update/<int:pk>/", views_address.AddressUpdateView.as_view(), name="address_update"),
     path("address-delete/<int:pk>/", views_address.AddressDeleteView.as_view(), name="address_delete"),
 ]
