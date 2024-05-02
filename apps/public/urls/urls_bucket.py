42d1b8bfe5a752cedd7a1acd51c4d67d69d08e00
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 23:13:35 +0330

    :sparkles:  Add simple url bucket

---
+++ apps/public/urls/urls_bucket.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from apps.product.views import views_template
+urlpatterns = [
+    # path('', BucketHomeView.as_view(), name='bucket_home'),
+    # path('delete_obj_bucket/<str:key>/', BucketDeleteObjView.as_view(), name='deleting_obj_bucket'),
+    # path('download_obj_bucket/<str:key>/', BucketDownloadObjView.as_view(), name='download_obj_bucket'),
+]
