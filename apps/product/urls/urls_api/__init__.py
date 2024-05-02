f930765db067631b9c192a4564c78c5518ced46c
pedramkarimii <pedram.9060@gmail.com>
2024-05-02 21:45:27 +0330

    :sparkles: Added API endpoints for product, category, brand, discount, and warehouse_keeper

---
+++ apps/product/urls/urls_api/__init__.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("", include("apps.product.urls.urls_api.product")),
+    path("", include("apps.product.urls.urls_api.category")),
+    path("", include("apps.product.urls.urls_api.brand")),
+    path("", include("apps.product.urls.urls_api.discount")),
+    path("", include("apps.product.urls.urls_api.warehouse_keeper")),
+]
