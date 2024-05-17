194830af1b987c203ec44705d5e9e45d090f0031
pedram <pedram.9060@gmail.com>
2024-05-18 00:27:56 +0330

    :zap: high complexity to low complexity, Refactored HomeView for improved readability and performance. Added separate methods for retrieving categories, products, applying discounts, and handling search queries. Optimized database queries and improved code reusabilit

--- apps/public/views/views_template/views_home.py
+++ apps/public/views/views_template/views_home.py
@@ -1,4 +1,5 @@
 from django.contrib.postgres.search import TrigramSimilarity
+from django.db.models import Q
 from django.views import View
 from django.utils import timezone
 from datetime import timedelta
@@ -20,51 +21,68 @@ class HomeView(View):
         self.form_class_search = forms.SearchForm  # noqa
         return super().setup(request, *args, **kwargs)
 
-    def get(self, request, *args, **kwargs):
-        categories = forms.Category.objects.all().filter(is_deleted=False)
-        products = forms.Product.objects.all().filter(is_deleted=False)
-        products_search = forms.Product.objects.all().filter(is_deleted=False)
-        form_search = self.form_class_search(request.GET)
-        last_week = timezone.now() - timedelta(days=7)
-        products_new = forms.Product.objects.all().filter(create_time__gte=last_week,is_deleted=False)
-        numerical_discount = None
-        percentage_discount = None
-        product_discount = None
+    def get_categories(self):  # noqa
+        """
+        Retrieve non-deleted categories.
+        """
+        return forms.Category.objects.filter(is_deleted=False, is_active=True)
+
+    def get_products(self):  # noqa
+        """
+        Retrieve non-deleted products.
+        """
+        return forms.Product.objects.filter(is_deleted=False, is_active=True, category__is_active=True)
+
+    def apply_discounts(self, products):  # noqa
+        """
+        Apply discounts to products if applicable.
+        """
         for product in products:
             product_price = product.price
-            latest_discount = product.product_code_discounts.filter(is_expired=False).order_by('-create_time').first()
+            latest_discount = product.product_code_discounts.filter(is_expired=False, is_active=True).order_by(
+                '-create_time').first()
+            numerical_discount = None
+            percentage_discount = None
             if latest_discount:
                 numerical_discount = latest_discount.numerical_discount
-                print('A' * 100, numerical_discount)
                 percentage_discount = latest_discount.percentage_discount
-                print('B' * 100, percentage_discount)
-                if numerical_discount and percentage_discount:
-                    product_numerical_discount = product_price - numerical_discount
-                    print('W' * 100, product_numerical_discount)
-                    product_percentage_discount = product_price - (product_price * percentage_discount / 100)
-                    print('X' * 100, product_percentage_discount)
-                    if product_numerical_discount < product_percentage_discount:
-                        product_discount = product_numerical_discount
-                        numerical_discount = None
-                        percentage_discount = None
-                    else:
-                        product_discount = product_percentage_discount
-                        numerical_discount = None
-                        percentage_discount = None
-                if percentage_discount:
-                    product_discount = product_price - (product_price * percentage_discount / 100)
+            if numerical_discount and percentage_discount:
+                product_numerical_discount = product_price - numerical_discount
+                product_percentage_discount = product_price - (product_price * percentage_discount / 100)
+                if product_numerical_discount < product_percentage_discount:
+                    product.discount = product_numerical_discount
+                else:
+                    product.discount = product_percentage_discount
+            elif percentage_discount:
+                product.discount = product_price - (product_price * percentage_discount / 100)
+            elif numerical_discount:
+                product.discount = product_price - numerical_discount
+            else:
+                product.discount = None
 
-                if numerical_discount:
-                    product_discount = product_price - numerical_discount
+    def get_products_search(self, form_search):  # noqa
+        """
+        Retrieve products based on search query.
+        """
+        products_search = forms.Product.objects.all().filter(Q(is_deleted=False), Q(is_active=True),
+                                                             Q(category__is_active=True))
+        search_query = form_search.cleaned_data.get('search')
+        products = products_search.annotate(
+            similarity=TrigramSimilarity('name', search_query) + TrigramSimilarity('description', search_query)
+        ).filter(similarity__gt=0.1).order_by('-similarity')
+        return products
 
-                if not product_discount and not numerical_discount:
-                    product_discount = None
-            product.discount = product_discount
+    def get(self, request, *args, **kwargs):
+        categories = self.get_categories()
+        products = self.get_products()
+        products_search = products
+        form_search = self.form_class_search(request.GET)
+        last_week = timezone.now() - timedelta(days=7)
+        products_new = products.filter(create_time__gte=last_week, is_deleted=False)
+        if self.apply_discounts is not None:
+            self.apply_discounts(products)  # noqa
         if form_search.is_valid():
-            search_query = form_search.cleaned_data.get('search')
-            products_search = products_search.annotate(
-                similarity=TrigramSimilarity('name', search_query) + TrigramSimilarity('description', search_query)
-            ).filter(similarity__gt=0.1).order_by('-similarity')
+            products_search = self.get_products_search(form_search)
         admin_permissions = None
         admin_or_supervisor = None
         admin_or_seller = None
@@ -82,7 +100,6 @@ class HomeView(View):
             'permissions': permissions,
             'categories': categories,
             'products': products_search,
-            'discount': product_discount,
             'products_new': products_new,
             'form_search': form_search,
         })
