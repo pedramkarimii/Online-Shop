5220870c21d11e97cd2d30b1149ed5b7f87b40c7
pedram <pedram.9060@gmail.com>
2024-05-17 23:54:45 +0330

    :bug: Change comment URL product

--- apps/product/urls/urls_bucket/__init__.py
+++ apps/product/urls/urls_bucket/__init__.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("apps.product.urls.urls_bucket.product")),
+    # path("", include("apps.product.urls.urls_bucket.product")),
     path("", include("apps.product.urls.urls_bucket.category")),
     path("", include("apps.product.urls.urls_bucket.brand")),
     path("", include("apps.product.urls.urls_bucket.discount")),
