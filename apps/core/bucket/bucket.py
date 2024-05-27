2ef647908af71bfd39f30a64e4dc139c82e150dc
pedram <pedram.9060@gmail.com>
2024-05-27 20:24:35 +0330

    :sparkles: Add a Bucket class to interact with AWS S3, with methods to get, delete, and download objects, utilizing boto3 and Django settings.

--- apps/core/bucket/bucket.py
+++ apps/core/bucket/bucket.py
@@ -1,15 +1,19 @@
 import boto3
 from django.conf import settings
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class Bucket:
     def __init__(self):
         session = boto3.session.Session()
-        self.connection_s3_client = session.client(service_name=settings.AWS_SERVICE_NAME,
-                                                   aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                                                   aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-                                                   endpoint_url=settings.AWS_S3_ENDPOINT_URL
-                                                   )
+        self.connection_s3_client = session.client(
+            service_name=settings.AWS_SERVICE_NAME,
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            endpoint_url=settings.AWS_S3_ENDPOINT_URL
+        )
 
     def get_object(self):
         result = self.connection_s3_client.list_objects_v2(Bucket=settings.AWS_STORAGE_BUCKET_NAME)
@@ -20,11 +24,23 @@ class Bucket:
 
     def delete_object(self, key):
         self.connection_s3_client.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=key)
-        return True
+        try:
+            self.connection_s3_client.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=key)
+            return True
+        except Exception as e:
+            logging.error(f"Error deleting object from bucket: {e}")
+            return False
 
     def download_object(self, key):
-        with open(settings.AWS_LOCAL_STORAGE + key, 'wb') as f:
-            self.connection_s3_client.download_fileobj(settings.AWS_STORAGE_BUCKET_NAME, key, f)
+        local_file_path = settings.AWS_LOCAL_STORAGE + key
+        try:
+            with open(local_file_path, 'wb') as f:
+                self.connection_s3_client.download_fileobj(settings.AWS_STORAGE_BUCKET_NAME, key, f)
+            logger.info(f"Downloaded object '{key}' to '{local_file_path}'")
+            return local_file_path
+        except Exception as e:
+            logger.error(f"Error downloading object '{key}': {e}")
+            return None
 
 
 buckets = Bucket()
