372d581b8407b142d2a1812e0ab43a48a0712068
pedram <pedram.9060@gmail.com>
2024-05-27 20:28:17 +0330

    :sparkles: Add a Bucket class to interact with AWS S3, with methods to get, delete, and download objects, utilizing boto3 and Django settings.

--- apps/core/bucket/bucket_views.py
+++ apps/core/bucket/bucket_views.py
@@ -1,32 +1,31 @@
 from django.shortcuts import render, redirect
-from django.views import View
+from apps.core.permission.template_permission_admin import CRUD
 from .tasks import delete_object_task, all_buckets_objects_task, download_object_task
 from django.contrib import messages
 
 
-class BucketView(View):
+class BucketView(CRUD.AdminPermissionRequiredMixinView):
     template_name = 'bucket/bucket.html'
     http_method_names = ['get']
 
     def get(self, request):
         objects = all_buckets_objects_task()
-        print('A' * 100, objects)
         return render(request, self.template_name, {'objects': objects})
 
 
-class BucketDeleteObjView(View):
+class BucketDeleteObjView(CRUD.AdminPermissionRequiredMixinView):
     http_method_names = ['get', 'delete']
 
     def get(self, request, key):
         delete_object_task.delay(key)
         messages.success(request, 'Your objects will be delete soon', extra_tags='info')
-        return redirect('bucket_home')
+        return redirect('bucket_main')
 
 
-class BucketDownloadObjView(View):
+class BucketDownloadObjView(CRUD.AdminPermissionRequiredMixinView):
     http_method_names = ['get']
 
     def get(self, request, key):
         download_object_task.delay(key)
         messages.success(request, 'Your download will be start soon', extra_tags='info')
-        return redirect('bucket_home')
+        return redirect('bucket_main')
