9dd06eeb2c30b2bf547c631750e22a79e6a56b5d
pedram <pedram.9060@gmail.com>
2024-05-27 20:41:42 +0330

    :sparkles: Updated URL patterns to use more descriptive and consistent paths, incorporating re_path for dynamic URL parameters.

--- apps/core/bucket/urls.py
+++ apps/core/bucket/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
+from django.urls import path, re_path
 from .bucket_views import BucketView, BucketDeleteObjView, BucketDownloadObjView
 
 urlpatterns = [
-    path('', BucketView.as_view(), name='bucket_main'),
-    path('delete_obj_bucket/<str:key>/', BucketDeleteObjView.as_view(), name='deleting_obj_bucket'),
-    path('download_obj_bucket/<str:key>/', BucketDownloadObjView.as_view(), name='download_obj_bucket'),
+    path('showbucket', BucketView.as_view(), name='bucket_main'),
+    re_path(r'^download_obj_bucket/(?P<key>.+)/$', BucketDownloadObjView.as_view(), name='download_obj_bucket'),
+    re_path(r'^delete_obj_bucket/(?P<key>.+)/$', BucketDeleteObjView.as_view(), name='deleting_obj_bucket'),
 ]
