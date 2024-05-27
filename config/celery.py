3a8ae244a95891facae8a8bd6a9df59555c8fc6a
pedram <pedram.9060@gmail.com>
2024-05-27 20:33:57 +0330

    :sparkles: Add  Celery configuration, including Django settings integration, task autodiscovery, logging setup, and set Redis as the broker and result backend.

--- config/celery.py
+++ config/celery.py
@@ -1,16 +1,17 @@
 import os
 from celery import Celery
-from datetime import timedelta
+import logging
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-app = Celery("config")
+app = Celery("config", base_url="http://localhost")
+
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Load all tasks
 app.autodiscover_tasks()
-app.conf.task_serializer = 'json'
-app.conf.result_serializer = 'pickle'
-app.conf.accept_content = ['json', 'pickle']
-app.conf.result_expires = timedelta(days=1)
-app.conf.task_always_eager = False
-app.conf.worker_prefetch_multiplier = 1
+
+log_file = "/var/log/celery.log"
+logging.basicConfig(filename=log_file, level=logging.DEBUG)
+
+app.conf.broker_url = 'redis://127.0.0.1:6379/0'
+app.conf.result_backend = 'redis://127.0.0.1:6379/0'
