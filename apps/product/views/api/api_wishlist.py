07db743c24d31d0beda06ba3b8f4fe91ebdd2596
pedram <pedram.9060@gmail.com>
2024-06-19 01:14:46 +0330

    :zap: Implemented APIs for wishlist management including adding, showing, updating, and deleting products, as well as applying discounts.

--- apps/product/views/api/api_wishlist.py
+++ apps/product/views/api/api_wishlist.py
@@ -1,11 +1,19 @@
 import json
+from django.core.signing import Signer
+from django.db import transaction
+from django.db.models import Q
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from apps.account.models import UserAuth, Role, CodeDiscount
+from apps.account.users_auth.authenticate import JWTAuthentication
+from apps.account.users_auth.services import update_user_auth_uuid
 from apps.product.form_data import forms
 from rest_framework import status, views
 from apps.product.form_data import serializers
-from django.utils.translation import gettext_lazy as _
 from apps.product import mixin
+from apps.product.mixin import ProductDiscountMixin
 
 
 class WishlistAddProductAPI(views.APIView, mixin.ProductDiscountMixin):
@@ -38,7 +46,7 @@ class WishlistShowProductAPI(views.APIView):
     def show_product_from_wishlist_cookie(self, request):  # noqa
         wishlist_data = {}
         for key, value in request.COOKIES.items():
-            if key.startswith('product_'):
+            if key.startswith('product_wishlist'):
                 product_data = json.loads(value)
                 wishlist_data[key] = product_data
         return JsonResponse(wishlist_data, status=status.HTTP_200_OK)
@@ -47,3 +55,168 @@ class WishlistShowProductAPI(views.APIView):
         wishlist_items = forms.Wishlist.objects.filter(user=request.user)
         serializer = serializers.WishlistProductSerializer(wishlist_items, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+
+class WishlistUpdateProductAPI(views.APIView):
+    def setup(self, request, *args, **kwargs):
+        """Initialize the success_url."""  # noqa
+        self.product_instance = get_object_or_404(forms.Product, pk=kwargs['pk'])  # noqa
+        self.signer = Signer()  # noqa
+        self.user_authenticated = request.user.is_authenticated  # noqa
+        self.signed_product_id = self.signer.sign(str(self.product_instance.pk))  # noqa
+        self.latest_discount = self.product_instance.product_code_discounts.filter(is_expired=False,  # noqa
+                                                                                   is_active=True).order_by(
+            '-create_time').first()
+        self.request_quantity = request.POST.get('quantity')  # noqa
+        self.request_total_price = request.POST.get('total_price')  # noqa
+        self.form_class = forms.WishlistAddForm  # noqa
+        return super().setup(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.update_product_from_wishlist_cookie(request)
+
+        return self.update_product_from_wishlist_authenticated(request)
+
+    def update_product_from_wishlist_cookie(self, request):  # noqa
+        new_quantity = int(self.request_quantity)
+        new_total_price = int(self.request_total_price)
+        calculate = ProductDiscountMixin()
+        product_discount = calculate.calculate_product_discount(self.product_instance, self.latest_discount)
+        cookie_key = f"product_wishlist{self.signed_product_id}"
+        if cookie_key in self.request.COOKIES:
+            # product_data = json.loads(request.COOKIES[cookie_key])
+            product_data = {
+                'product': self.product_instance.pk,
+                'name': self.product_instance.name,
+                'price': product_discount if product_discount else self.product_instance.price,
+                'quantity': new_quantity,
+                'total_price': new_total_price
+            }
+            print(product_data)
+
+            product_json = json.dumps(product_data)
+            print(product_json)
+            response = JsonResponse({'success': True})
+            response.set_cookie(cookie_key, product_json, max_age=604800)
+            response.status_code = status.HTTP_200_OK
+        else:
+            response = JsonResponse({'success': False})
+            response.status_code = status.HTTP_404_NOT_FOUND
+
+        return response
+
+    def update_product_from_wishlist_authenticated(self, request):  # noqa
+        wishlist_items = forms.Wishlist.objects.filter(user=request.user)
+        serializer = serializers.WishlistProductSerializer(wishlist_items, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+
+class WishlistDeleteProductAPI(views.APIView):
+    def setup(self, request, *args, **kwargs):
+        """Initialize the success_url."""  # noqa
+        self.product_instance = get_object_or_404(forms.Product, pk=kwargs['pk'])  # noqa
+        self.signer = Signer()  # noqa
+        self.user_authenticated = request.user.is_authenticated  # noqa
+        self.signed_product_id = self.signer.sign(str(self.product_instance.pk))  # noqa
+        return super().setup(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):  # noqa
+        if not request.user.is_authenticated:
+            return self.delete_product_from_wishlist_cookie(request)
+
+        return self.delete_product_from_wishlist_authenticated(request)
+
+    def delete_product_from_wishlist_cookie(self, request):
+        cookie_key = f"product_wishlist{self.signed_product_id}"
+        if cookie_key in request.COOKIES:
+
+            response = JsonResponse({'success': True})
+            response.delete_cookie(cookie_key)
+        else:
+            response = JsonResponse({'success': False})
+        return response
+
+    def delete_product_from_wishlist_authenticated(self, request):  # noqa
+        product = self.product_instance
+        if self.user_authenticated:
+            with transaction.atomic():
+                wishlist_obj = forms.Wishlist.objects.filter(user=request.user, product=product).first()
+                wishlist_obj.delete()
+                return JsonResponse({'success': True})
+        else:
+            return JsonResponse({'success': False})
+
+
+class WishlistDiscountCodProductAPI(views.APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [JWTAuthentication]
+
+    def setup(self, request, *args, **kwargs):
+        """Initialize the success_url."""  # noqa
+        # print(f'User authenticated: {self.user_authenticated}')  # Debugging line
+        # print(f'User instance: {self.user_instance}')  # Debugging line
+        print(*[f'{k} : {v}' for k, v in request.META.items()], sep='\n')
+        self.user_instance = request.user.id  # noqa
+        self.user_authenticated = request.user.is_authenticated  # noqa
+        self.code_discounts_role = CodeDiscount.objects.filter(  # noqa
+            is_expired=False,
+            is_active=True
+        ).order_by('-create_time').first()
+        self.request_code_discount = request.POST.get('code_discount')  # noqa
+        self.request_total_price = request.POST.get('total_price')  # noqa
+        return super().setup(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if self.user_authenticated:
+            print(f'User authenticated: {self.user_authenticated}')  # Debugging line
+            print(f'User instance: {self.user_instance}')  # Debugging line
+            print(*[f'{k} : {v}' for k, v in request.META.items()], sep='\n')
+            return self.discount_cod_product_from_wishlist_cookie(request)
+        else:
+            print(f'User authenticated: {self.user_authenticated}')  # Debugging line
+            print(f'User instance: {self.user_instance}')  # Debugging line
+            print(*[f'{k} : {v}' for k, v in request.META.items()], sep='\n')
+            return JsonResponse({'success': False, 'message': 'User not authenticated'},
+                                status=status.HTTP_401_UNAUTHORIZED)
+
+    def discount_cod_product_from_wishlist_cookie(self, request):
+        code_discount = self.code_discounts_role.code
+        user_has_discount = Role.objects.filter(
+            code_discount__code=code_discount,
+            is_deleted=False,
+            is_active=True
+        ).filter(
+            Q(golden=self.user_instance) | Q(silver=self.user_instance) | Q(bronze=self.user_instance) | Q(
+                seller=self.user_instance)
+        ).exists()
+
+        if user_has_discount and code_discount == self.request_code_discount:
+            new_total_price = int(self.request_total_price)
+            calculate = ProductDiscountMixin()
+            product_discount = calculate.calculate_product_discount(new_total_price, self.code_discounts_role)
+            wishlist_qs = forms.Wishlist.objects.filter(user=self.user_instance).first()
+
+            if wishlist_qs:
+                with transaction.atomic():
+                    wishlist_qs.total_price = product_discount
+                    wishlist_qs.save()
+                    return JsonResponse({'success': True}, status=status.HTTP_200_OK)
+            else:
+                return JsonResponse({'success': False, 'message': 'Wishlist not found'},
+                                    status=status.HTTP_404_NOT_FOUND)
+        else:
+            return JsonResponse({'success': False, 'message': 'Discount not applicable or invalid code'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+    # def transition_to_authentication(self, request):
+    #     # Save the discount code and total price in the session for later use
+    #     request.session['code_discount'] = self.request_code_discount
+    #     request.session['total_price'] = self.request_total_price
+    #
+    #     # Redirect to the login page with the next parameter set to the current URL
+    #     login_url = f"{reverse('login')}?next={request.get_full_path()}"
+    #     return redirect(login_url)
+
+
+
