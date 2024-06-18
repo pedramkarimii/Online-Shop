01a5378d983be9321542bb6c006def64bc23702d
pedram <pedram.9060@gmail.com>
2024-06-19 01:16:58 +0330

    :zap: Add wishlist discount view to account app URLs

--- apps/account/urls/urls_template/discount_cod.py
+++ apps/account/urls/urls_template/discount_cod.py
@@ -3,8 +3,11 @@ from apps.account.views.views_template import views_discount
 
 urlpatterns = [
     path('discount-cod-create/', views_discount.DiscountCodCreateView.as_view(), name='discount_cod_create'),
-    path('admin-discount-cod-list/', views_discount.AdminDiscountCodDetailView.as_view(), name='admin_discount_cod_list'),
+    path('admin-discount-cod-list/', views_discount.AdminDiscountCodDetailView.as_view(),
+         name='admin_discount_cod_list'),
     path('discount-cod-detail/<int:pk>/', views_discount.DiscountCodDetailView.as_view(), name='discount_cod_detail'),
     path('discount-cod-update/<int:pk>/', views_discount.DiscountCodUpdateView.as_view(), name='discount_cod_update'),
     path('discount-cod-delete/<int:pk>/', views_discount.DiscountCodDeleteView.as_view(), name='discount_cod_delete'),
+    path('discount-cod-wishlist/', views_discount.WishlistDiscountCodProductView.as_view(),
+         name='discount_cod_wishlist'),
 ]
