e7ae2c1576db5d8b52364144e2aad40855a6dda9
pedram <pedram.9060@gmail.com>
2024-06-19 01:47:29 +0330

    :zap: Refactored wishlist API URLs, add discount code endpoint and wishlists delete.

--- apps/product/urls/urls_api/wishlist.py
+++ apps/product/urls/urls_api/wishlist.py
@@ -4,9 +4,9 @@ from apps.product.views.api import api_wishlist
 urlpatterns = [
 
     path('wishlists-add/<int:pk>/', api_wishlist.WishlistAddProductAPI.as_view(), name='wishlist_add_api'),
-    # path('admin-seller-wishlist-list/', api_wishlist.AdminOrSellerProductListView.as_view(),
-    #      name='admin_or_seller_wishlist_list'),
     path('wishlists-detail/<int:pk>/', api_wishlist.WishlistShowProductAPI.as_view(), name='wishlist_detail_api'),
     path('wishlists-update/<int:pk>/', api_wishlist.WishlistUpdateProductAPI.as_view(), name='wishlist_update_api'),
-    # path('wishlist-delete/<int:pk>/', api_wishlist.WishlistDeleteView.as_view(), name='wishlist_delete'),
+    path('wishlists-delete/<int:pk>/', api_wishlist.WishlistDeleteProductAPI.as_view(), name='wishlist_delete_api'),
+    path('wishlists-discount-cod/', api_wishlist.WishlistDiscountCodProductAPI.as_view(),
+         name='wishlist_discount_cod_api'),
 ]
