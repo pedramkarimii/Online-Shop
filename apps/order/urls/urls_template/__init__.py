fa75f75ed5f687ac7a3c4449a0dcdb7699ead651
pedram <pedram.9060@gmail.com>
2024-06-20 01:19:05 +0330

    :zap: Update Url change checkout to payment

--- apps/order/urls/urls_template/__init__.py
+++ apps/order/urls/urls_template/__init__.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("apps.order.urls.urls_template.checkout")),
+    path("", include("apps.order.urls.urls_template.payment")),
     path("", include("apps.order.urls.urls_template.order_item")),
     path("", include("apps.order.urls.urls_template.order")),
 ]
