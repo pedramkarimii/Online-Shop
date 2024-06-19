05c4d0380abfea34f9e6832cd811e14ba87e7dc6
pedram <pedram.9060@gmail.com>
2024-06-20 01:18:54 +0330

    :zap: Update Url change checkout to payment

--- apps/order/urls/urls_api/__init__.py
+++ apps/order/urls/urls_api/__init__.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("apps.order.urls.urls_api.checkout")),
+    path("", include("apps.order.urls.urls_api.payment")),
     path("", include("apps.order.urls.urls_api.order_item")),
 ]
