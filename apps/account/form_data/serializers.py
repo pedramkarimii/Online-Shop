cf41c16ede4f5a3a4f0bab46cd80ab44540f1795
pedram <pedram.9060@gmail.com>
2024-06-30 22:52:20 +0330

    :bulb: Add explain cod

--- apps/account/form_data/serializers.py
+++ apps/account/form_data/serializers.py
@@ -5,11 +5,23 @@ from apps.account.users_auth.token import verify_token, refresh_access_token
 
 
 class LoginByPasswordSerializer(serializers.Serializer):
+    """
+    Serializer for handling user login via password. It requires two fields:
+    - `username`: The username of the user.
+    - `password`: The password of the user.
+    """
     username = serializers.CharField()
     password = serializers.CharField()
 
 
 class TokenVerifySerializer(serializers.Serializer):
+    """
+    Serializer for verifying a provided token. It contains:
+    - `token`: The token to be verified.
+
+    The `validate_token` method is used to check if the provided token is valid.
+    If the token is not valid, a `ValidationError` is raised.
+    """
     token = serializers.CharField()
 
     def validate_token(self, token):
@@ -19,10 +31,21 @@ class TokenVerifySerializer(serializers.Serializer):
 
 
 class RefreshTokenSerializer(serializers.Serializer):
+    """
+    Serializer for handling token refresh requests. It contains:
+    - `refresh_token`: The refresh token used to obtain a new access token.
+    """
     refresh_token = serializers.CharField()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the User model. It defines which fields of the User model
+    should be included in the serialized representation:
+    - `id`: The unique identifier of the user.
+    - `username`: The username of the user.
+    - `email`: The email address of the user.
+    """
     class Meta:
         model = User
         fields = ("id", "username", "email")
