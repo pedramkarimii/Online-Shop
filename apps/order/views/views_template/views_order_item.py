76ebbf7f5880e6de5b8ed87be7ffcb2e5e0f96e1
pedram <pedram.9060@gmail.com>
2024-07-01 15:40:02 +0330

    :bulb: Add explain cod

--- apps/order/views/views_template/views_order_item.py
+++ apps/order/views/views_template/views_order_item.py
@@ -11,10 +11,25 @@ from apps.product.models import Product
 
 
 class AddOrderItemView(mixin.ProductDiscountMixin):
+    """
+    Defines a view for adding items to a user's shopping cart, inheriting from `ProductDiscountMixin` for discount
+     calculation functionality.
+    """
 
     def add_product_to_cart_authenticated(self):  # noqa
-        form = self.form_class(self.request_post)
+        """
+        Handles adding a product to the cart for authenticated users. It initializes a form instance using POST data.
 
+        Checks if the form data is valid. If valid, calculates the discounted price for the product if a discount is
+        available; otherwise, uses the product's regular price.
+
+        Attempts to retrieve an existing cart item for the user and product. If not found, creates a new cart item
+        with default quantity 1 and calculated total price.
+
+        If form validation fails, returns JSON response with error details. If successful, updates cart item
+        quantity and total price if item exists, or creates a new item.
+        """
+        form = self.form_class(self.request_post)
         if form.is_valid():
             try:
                 product_discount = self.calculate_product_discount(self.product_instance,
@@ -43,13 +58,25 @@ class AddOrderItemView(mixin.ProductDiscountMixin):
 
 
 class ShowOrderItemProductView(generic.ListView):
+    """
+    Defines a view for displaying items in the shopping cart, using a ListView for rendering.
+    """
+
     def get(self, request, *args, **kwargs):  # noqa
+        """
+        Handles GET requests. If the user is not authenticated, calls a method to display cart items stored in cookies.
+         Otherwise, displays items stored in the database.
+        """
         if not request.user.is_authenticated:
             return self.show_product_order_item_cookie(request)
         else:
             return self.show_product_order_item_authenticated(request)
 
     def show_product_order_item_cookie(self, request):  # noqa
+        """
+        Fetches and displays cart items stored in cookies for non-authenticated users. Renders a template
+        with cart item details and total price.
+        """
         cart_items_cookies = {}
         sum_total_price = 0
         img_url = set()
@@ -70,6 +97,10 @@ class ShowOrderItemProductView(generic.ListView):
                        'sum_total_price': sum_total_price})
 
     def show_product_order_item_authenticated(self, request):  # noqa
+        """
+        Fetches and displays cart items stored in the database for authenticated users. Calculates any available
+         product discounts and renders a template with cart item details and total price.
+        """
         cart_items = forms.OrderItem.objects.filter(user=request.user)  # noqa
         cart_data = {}
         sum_total_price = 0
@@ -97,8 +128,15 @@ class ShowOrderItemProductView(generic.ListView):
 
 
 class DeleteOrderItemProductView(generic.ListView):
+    """
+    Defines a view for deleting items from the shopping cart, using a ListView for rendering.
+    """
+
     def setup(self, request, *args, **kwargs):
-        """Initialize the success_url."""  # noqa
+        """
+        Initializes necessary variables and retrieves the product instance based on the URL parameter `pk`.
+        Uses the `Signer` to sign the product ID for secure retrieval.
+        """
         self.product_instance = get_object_or_404(Product, pk=kwargs['pk'])  # noqa
         self.signer = Signer()  # noqa
         self.user_authenticated = request.user.is_authenticated  # noqa
@@ -106,12 +144,20 @@ class DeleteOrderItemProductView(generic.ListView):
         return super().setup(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):  # noqa
+        """
+        Handles POST requests. If the user is not authenticated, calls a method to delete the product from the cart
+         stored in cookies. Otherwise, deletes the product from the database.
+        """
         if not self.user_authenticated:
             return self.delete_product_from_cart_cookie(request)
         else:
             return self.delete_product_from_cart_authenticated(request)
 
     def delete_product_from_cart_authenticated(self, request):
+        """
+        Deletes the specified product from the cart for authenticated users. Uses an atomic transaction
+        to ensure database integrity and returns a JSON response indicating success or failure.
+        """
         product = self.product_instance
         if self.user_authenticated:
             with transaction.atomic():
@@ -122,6 +168,10 @@ class DeleteOrderItemProductView(generic.ListView):
             return JsonResponse({'success': False})
 
     def delete_product_from_cart_cookie(self, request):
+        """
+        Deletes the specified product from the cart stored in cookies for non-authenticated users.
+        Returns a JSON response indicating success or failure and deletes the corresponding cookie.
+        """
         cookie_key = f"product_cart{self.signed_product_id}"
         if cookie_key in self.request.COOKIES:
             response = JsonResponse({'success': True})
@@ -132,8 +182,16 @@ class DeleteOrderItemProductView(generic.ListView):
 
 
 class UpdateOrderItemProductView(AddOrderItemView):
+    """
+    Defines a view for updating items in the shopping cart, inheriting functionality from
+    `AddOrderItemView` for adding products to the cart.
+    """
     def setup(self, request, *args, **kwargs):
-        """Initialize the success_url."""  # noqa
+        """
+        Initializes necessary variables and retrieves the product instance based on the URL
+        parameter `pk`. Uses the `Signer` to sign the product ID for secure retrieval. Also initializes form data,
+         quantity, and total price for updating the cart item.
+        """
         self.product_instance = get_object_or_404(Product, pk=kwargs['pk'])  # noqa
         self.signer = Signer()  # noqa
         self.user_authenticated = request.user.is_authenticated  # noqa
@@ -147,13 +205,20 @@ class UpdateOrderItemProductView(AddOrderItemView):
         return super().setup(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):  # noqa
+        """
+        Handles POST requests. If the user is not authenticated, calls a method to update the
+         product in the cart stored in cookies. Otherwise, updates the product in the database.
+        """
         if not self.user_authenticated:
             return self.update_product_from_cart_cookie(request)
         else:
             return self.update_product_from_cart_authenticated(request)
 
     def update_product_from_cart_authenticated(self, request):
-
+        """
+        Updates the specified product in the cart for authenticated users. Uses an atomic
+        transaction to ensure database integrity and returns a JSON response indicating success or failure.
+        """
         if self.user_authenticated:
             new_quantity = int(self.request_quantity)
             new_total_price = int(self.request_total_price)
@@ -168,6 +233,11 @@ class UpdateOrderItemProductView(AddOrderItemView):
             return JsonResponse({'success': False})
 
     def update_product_from_cart_cookie(self, request):
+        """
+        Updates the specified product in the cart stored in cookies for non-authenticated users.
+         Calculates any available product discounts, updates the cookie with the new product data,
+         and returns a JSON response indicating success or failure.
+        """
         new_quantity = int(self.request_quantity)
         new_total_price = int(self.request_total_price)
 
