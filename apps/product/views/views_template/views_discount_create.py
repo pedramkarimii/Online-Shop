4e871f4f0f67c49ba443b9092fc842b8689ad884
pedram <pedram.9060@gmail.com>
2024-07-13 21:00:02 +0330

    :memo: Add explain cod

--- apps/product/views/views_template/views_discount_create.py
+++ apps/product/views/views_template/views_discount_create.py
@@ -9,10 +9,15 @@ from apps.core.permission.template_permission_admin import CRUD
 
 
 class DiscountCreateView(CRUD.AdminPermissionRequiredMixinView):
+    """
+    Create a new discount.
+    """
     http_method_names = ['get', 'post']
 
     def setup(self, request, *args, **kwargs):
-        """Initialize the success_url."""
+        """
+        function to set up the view before rendering.
+        """
         self.form_class = forms.DiscountCreateForm  # noqa
         self.next_page_discount_create = reverse_lazy('discount_create')  # noqa
         self.template_discount_create = 'product/discount/discount_create.html'  # noqa
@@ -20,16 +25,26 @@ class DiscountCreateView(CRUD.AdminPermissionRequiredMixinView):
         return super().setup(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
+        """
+        function to handle get request on the view. Renders the form for creating a new discount.
+        """
         return render(request, self.template_discount_create, {'form': self.form_class()})
 
     def post(self, request, *args, **kwargs):
+        """
+        function to handle post request on the view. Creates a new discount based on the form data.
+        Validates the form data and saves the discount if the form is valid. If the form is invalid,
+        it renders the form with error messages.
+        :param request: The HTTP request object
+        :param args: Positional arguments
+        :param kwargs: Keyword arguments
+        :return: The rendered template response
+        """
         form = self.form_class(self.request_post)  # noqa
 
         if form.is_valid():
             percentage_discount = form.cleaned_data.get('percentage_discount')
             numerical_discount = form.cleaned_data.get('numerical_discount')
-            # product = form.cleaned_data.get('product')
-            # category = form.cleaned_data.get('category')
 
             if not percentage_discount and not numerical_discount:
                 messages.error(request, _('Either percentage discount or numerical discount must be provided.'),
@@ -38,12 +53,6 @@ class DiscountCreateView(CRUD.AdminPermissionRequiredMixinView):
             if percentage_discount and numerical_discount:
                 messages.error(request, _('Only one type of discount can be provided.'), extra_tags='error')
                 return redirect(self.next_page_discount_create)
-            # if forms.Product.objects.all().product_code_discounts(product=product).exists():
-            #     messages.error(request, _(f'Discount product already exists.'), extra_tags='error')
-            #     return redirect(self.next_page_discount_create)
-            # if forms.Discount.objects.filter(category=category).exists():
-            #     messages.error(request, _(f'Discount category already exists.'), extra_tags='error')
-            #     return redirect(self.next_page_discount_create)
             discount = form.save(commit=False)
             discount.save()
             messages.success(request, _(f'Discount created successfully Date:{discount.expiration_date} .'),
@@ -55,18 +64,30 @@ class DiscountCreateView(CRUD.AdminPermissionRequiredMixinView):
 
 
 class AdminDiscountListView(CRUD.AdminPermissionRequiredMixinView, generic.ListView):
+    """
+    List all discounts for admin or supervisor.
+    """
     http_method_names = ['get']  # noqa
     model = forms.Discount
 
     def setup(self, request, *args, **kwargs):
+        """
+        function to set up the view before rendering.
+        """
         self.context_object_name = 'discounts'
         self.template_name = 'user/detail/admin/admin_discount.html'
         return super().setup(request, *args, **kwargs)
 
     def get_queryset(self):
+        """
+        function to get the queryset for the view.
+        """
         return forms.Discount.objects.all()
 
     def get_context_data(self, **kwargs):
+        """
+        function to get the context data for the view.
+        """
         context = super().get_context_data(**kwargs)  # noqa
         discounts = forms.Discount.objects.all()
         if self.request.user.is_superuser or self.request.user.is_staff:
@@ -79,16 +100,24 @@ class AdminDiscountListView(CRUD.AdminPermissionRequiredMixinView, generic.ListV
 
 
 class DiscountDetailView(CRUD.AdminPermissionRequiredMixinView, DetailView):
+    """
+    Detail view for a discount.
+    """
     http_method_names = ['get']  # noqa
     model = forms.Discount
 
     def setup(self, request, *args, **kwargs):
+        """
+        function to set up the view before rendering.
+        """
         self.context_object_name = 'address'
         self.template_name = 'product/discount/discount.html'
         return super().setup(request, *args, **kwargs)
 
     def get_object(self, queryset=None):
-        """Return the discount instance based on the URL kwargs."""  # noqa
+        """
+        function to get the object for the view.
+        """
         queryset = self.get_queryset()  # noqa
         pk = self.kwargs.get(self.pk_url_kwarg)
         if pk is not None:
@@ -100,6 +129,11 @@ class DiscountDetailView(CRUD.AdminPermissionRequiredMixinView, DetailView):
         return obj
 
     def get_context_data(self, **kwargs):
+        """
+        function to get the context data for the view.
+        :param kwargs: Keyword arguments
+        :return: The context data for the view
+        """
         context = super().get_context_data(**kwargs)
         discounts = forms.Discount.objects.all()
         context['discounts'] = discounts
@@ -107,20 +141,36 @@ class DiscountDetailView(CRUD.AdminPermissionRequiredMixinView, DetailView):
 
 
 class DiscountUpdateView(CRUD.AdminPermissionRequiredMixinView):
+    """
+    Update an existing discount.
+    """
 
     def setup(self, request, *args, **kwargs):
-        """Initialize the success_url and retrieve the discount instance."""
-
+        """
+        function to set up the view before rendering.
+        """
         self.form_class = forms.DiscountUpdateForm  # noqa
         self.template_discount_update = 'product/discount/discount_update.html'  # noqa
         self.request_post = request.POST  # noqa
         return super().setup(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
+        """
+        function to handle get request on the view. Renders the form for updating an existing discount.
+        """
         form = self.form_class(instance=self.discount_instance)
         return render(request, self.template_discount_update, {'form': form, 'discount': self.discount_instance})
 
     def post(self, request, *args, **kwargs):
+        """
+        function to handle post request on the view. Updates an existing discount based on the form data.
+        Validates the form data and saves the discount if the form is valid. If the form is invalid,
+        it renders the form with error messages.
+        :param request: The HTTP request object
+        :param args: Positional arguments
+        :param kwargs: Keyword arguments
+        :return: The rendered template response
+        """
         form = self.form_class(self.request_post, instance=self.discount_instance)  # noqa
         if form.is_valid():  # noqa
             discount = form.save(commit=False)
