f3a7629c35c39d972473311e2d2e24dfe28f6909
pedram <pedram.9060@gmail.com>
2024-07-13 22:54:46 +0330

    :memo: Add explain cod

--- apps/order/views/views_template/views_order.py
+++ apps/order/views/views_template/views_order.py
@@ -1,37 +1,64 @@
-from django.core.signing import Signer
 from django.db import transaction
+from django.db.models import Q
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
 from django.utils.translation import gettext_lazy as _
+from apps.account.models import CodeDiscount, Role, Address
 from apps.order.form_data import forms
 from apps.order import mixin
 
 
 class AddOrderView(mixin.ProductDiscountMixin):
-    def setup(self, request, *args, **kwargs):
-        self.signer = Signer()  # noqa
+    """
+    Class-base view for add order
+    """
+    http_method_names = ['get', 'post']
+
+    def setup(self, request, *args, **kwargs):  # noqa
+        """
+        Function for setup data to view
+        """
+        super().setup(request, *args, **kwargs)
+        self.next_page_payment_order = reverse_lazy('payment_order')  # noqa
         self.template_order = 'order/order/add_order.html'  # noqa
         self.user_is_authenticated = request.user.is_authenticated  # noqa
         self.user = request.user  # noqa
+        self.user_id = request.user.id  # noqa
+
+        self.code_discounts_role = CodeDiscount.objects.filter(  # noqa
+            is_expired=False,
+            is_active=True
+        ).order_by('-create_time').first()
+        self.latest_discount = self.code_discounts_role.code  # noqa
+        self.user_has_discount = Role.objects.filter(  # noqa
+            code_discount__code=self.latest_discount,
+            is_deleted=False,
+            is_active=True
+        ).filter(
+            Q(golden=self.user_id) | Q(silver=self.user_id) | Q(bronze=self.user_id) | Q(
+                seller=self.user_id)
+        ).exists()
         self.form_class = forms.OrderForm  # noqa
-        return super().setup(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
+        """
+        Handle GET requests: instantiate a blank version of the form.
+        """
         order_item = forms.OrderItem.objects.filter(user=self.user)  # noqa
         cart_data = {}
         sum_total_price = 0
         pk_product = None
         for item in order_item:
             total = item.product.price * item.quantity
-            product = item.product
+            product = item.product  # noqa
             pk_product = product.pk
             sum_total_price += item.total_price
             latest_discount_product_price = product.product_code_discounts.filter(is_expired=False,
                                                                                   is_active=True).order_by(
                 '-create_time').first()
-            calculate = mixin.ProductDiscountMixin()
-            product_discount = calculate.calculate_product_discount(product_instance=product,
-                                                                    latest_discount=latest_discount_product_price)
+            product_discount = self.calculate_product_discount(product_instance=product,
+                                                               latest_discount=latest_discount_product_price)
             cart_data[item.product.pk] = {
                 'product': item.product.id,
                 'image_url': product,
@@ -41,33 +68,44 @@ class AddOrderView(mixin.ProductDiscountMixin):
                 'total': total,
                 'total_price': item.total_price,
             }
-
+        form = self.form_class()
+        form.fields['address'].queryset = Address.objects.filter(user=self.user)
         return render(self.request, self.template_order,
-                      {'form': self.form_class(), 'pk_product': pk_product, 'products': cart_data,
+                      {'form': form, 'pk_product': pk_product, 'products': cart_data,
                        'sum_total_price': sum_total_price})
 
     @transaction.atomic
     def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests: instantiate a form instance with the passed
+        """
         form = self.form_class(self.request_post)
         if form.is_valid() and self.user_is_authenticated:
             return self.process_order(form)
         return JsonResponse({'success': False, 'message': _('You must be logged in to place an order')})
 
     def process_order(self, form):
+        """
+        function to process the order and save it to the database.
+        """
         order_items = forms.OrderItem.objects.filter(user=self.user)
-        product_discount = self.calculate_product_discount(self.product_instance, form.cleaned_data.get('quantity'))
         order = form.save(commit=False)
+        product_discount = form.cleaned_data.get('finally_price')
+        if self.user_has_discount:
+            product_discount = self.calculate_product_discount(form.cleaned_data.get('finally_price'),
+                                                               self.code_discounts_role)
         order.product = self.product_instance
         order.product_discount = product_discount
 
         if self.user_is_authenticated:
             order.user = self.request.user
             order.address = form.cleaned_data.get('address')
-            order.finally_price = form.cleaned_data.get('finally_price')
+            order.finally_price = product_discount
             order.save()
             order.order_item.set(order_items)
             order.save()
             response = JsonResponse({'success': True, 'message': _('Order added successfully')})
+            return redirect(self.next_page_payment_order)
         else:
             response = JsonResponse({'success': False, 'message': _('You must be logged in to place an order')})
 
